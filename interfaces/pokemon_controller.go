b117190442c5141f76a4b6752c8577f54c1329fa
Gerardo de Jesus Hernandez Perales <gerardo.hernandez@gerardondezsmbp.lan>
2022-04-06 23:11:18 -0500

    Small unsaved changes

--- interfaces/pokemon_controller.go
+++ interfaces/pokemon_controller.go
@@ -1,9 +1,15 @@
 package interfaces
 
+import "net/http"
+
 type PokemonController struct {
 	Logger Logger
 }
 
 func NewPokemonController(logger Logger) *PokemonController {
 	return &PokemonController{Logger: logger}
+}
+
+func (uc *PokemonController) Index(w http.ResponseWriter, r *http.Request) {
+	uc.Logger.LogAccess("access!!!")
 }
